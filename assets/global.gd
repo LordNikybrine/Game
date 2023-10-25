8131fb449e93cd5a95ecb8123309d1f919fa3641
Nick <nikybrine.jung@gmail.com>
2023-10-25 17:25:13 +0200

    updates1

--- assets/global.gd
+++ assets/global.gd
@@ -1,9 +1,6 @@
 extends Node
 
-var save_path = "res://popel.save"
-var file = FileAccess.open(save_path, FileAccess.READ)
-
-var level = file.get_var(level)
-var max_health = file.get_var(max_health)
+var level = 1
+var max_health = 100
 var spieler_health = 100
 var music_vol = 20
